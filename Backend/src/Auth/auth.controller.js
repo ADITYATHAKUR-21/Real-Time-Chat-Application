acde89ff792bc1f91a86d62555fea66c88ccb1a4
Aditya Thakur <adityakr123muz@gmail.com>
2026-07-02 09:29:33 +0530

    Reinstall mongoose adn jwt

--- Backend/src/Auth/auth.controller.js
+++ Backend/src/Auth/auth.controller.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs"
 import db from "../src/Config/db.js"
+import jwt from "jsonwebtoken"
 
 
-c
+export const register = async (req,res) => {
+    const {email, password, name} = req.body;
+
+    try {
+
+        const exitingUser = await User.findOne({email : "email"})
+        
+    } catch (error) {
+        console.log("error creating user ", error )
+        res.status(500).json({
+            error: "creating user error"
+        })
+    }
+
+};
